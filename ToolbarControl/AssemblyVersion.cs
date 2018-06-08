f1ea41f3f5533231edc4b9e4cc4a84ecacf4960a
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-06-08 16:16:23 -0400

    updated dependency check

--- ToolbarControl/AssemblyVersion.cs
+++ ToolbarControl/AssemblyVersion.cs
@@ -5,4 +5,4 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("0.1.6.8")]
+ [assembly: AssemblyVersion("0.1.6.9")]
--- ToolbarControl/Properties/AssemblyInfo.cs
+++ ToolbarControl/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: KSPAssembly("ToolbarController", 1, 0)]
+[assembly: KSPAssembly("ClickThroughBlocker", 1, 0)]
