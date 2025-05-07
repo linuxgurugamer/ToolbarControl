5a7e5d4b3fea19ae9060c9f6e195ed5670165ffb
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-05-06 20:36:48 -0400

    Update changelog and version

--- ToolbarControl/AssemblyVersion.cs
+++ ToolbarControl/AssemblyVersion.cs
@@ -10,5 +10,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("0.1.9.11")]
- [assembly: AssemblyFileVersion("0.1.9.11")]
+ [assembly: AssemblyVersion("0.1.9.14")]
+ [assembly: AssemblyFileVersion("0.1.9.14")]
